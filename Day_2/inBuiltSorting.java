d34daaa0cffc0d0e8adcd30131654078e1a86aba
nirabbaraha <nirabbaraha.dev@gmail.com>
2025-06-05 18:15:28 +0545

    DAY 2 of Java

---
+++ Day_2/inBuiltSorting.java
@@ -0,0 +1,13 @@
+package Day_2;
+
+import java.util.Arrays;
+
+public class inBuiltSorting {
+    public static void main(String[] args) {
+        int arr[]={2,1,3,4,5,};
+        //sorting function
+        Arrays.sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+    
+}
