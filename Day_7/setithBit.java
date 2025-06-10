73016df595600dd0ba3c803454f5677cc134e21e
nirabbaraha <nirabbaraha.dev@gmail.com>
2025-06-10 23:03:59 +0545

    Updated

--- Day_7/setithBit.java
+++ Day_7/setithBit.java
@@ -1,10 +1,11 @@
 package Day_7;
 
 public class setithBit {
-     public static int setIthBit(int n, int i) {
-        return n | (1 << i);
+     public static int setIthBit(int a, int i) {
+        return a | (1 << i);
     }
     public static void main(String[] args) {
         System.out.println(setIthBit(3, 3));
     }
 }
+  //here is the set bit
