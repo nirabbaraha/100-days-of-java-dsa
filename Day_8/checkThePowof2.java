f23fec8a457efc12f6883aa9c69f5b9af9bfb7c7
nirabbaraha <nirabbaraha.dev@gmail.com>
2025-06-11 21:23:02 +0545

    Day 8 of java

---
+++ Day_8/checkThePowof2.java
@@ -0,0 +1,11 @@
+package Day_8;
+
+public class checkThePowof2 {
+    public static boolean checkThePowof2(int n){
+        return n & (n-1)==0;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
