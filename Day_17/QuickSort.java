f02418bde512fcb622449210146405facb297beb
nirabbaraha <nirabbaraha.dev@gmail.com>
2025-07-03 08:00:40 +0545

    edited Quick Sort

--- Day_17/QuickSort.java
+++ Day_17/QuickSort.java
@@ -2,38 +2,48 @@ package Day_17;
 
 public class QuickSort {
 
-    public static void printArry(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+ " ");
+    public static void printArry(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        System.err.println();
+        System.out.println();
     }
-   public static void QuickSort(int arr[], int si, int ei){
-    int pidx=partition(arr,si,ei);
-    QuickSort(arr,si,pidx-1);
-    QuickSort(arr, pidx+1, ei);
-   }
-public static int partition(int arr[],int si, int ei){
-    int pivot= arr[ei];
-    int i=si-1;
-    for(int j=si;j<ei;j++){
-            if(arr[j]<=pivot){
-                i++;
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
-            }
 
-            int temp=pivot;
-            arr[ei]=arr[i];
-            arr[i]=temp;
-           
+    public static void QuickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int pidx = partition(arr, si, ei);
+        QuickSort(arr, si, pidx - 1);
+        QuickSort(arr, pidx + 1, ei);
     }
- return i;
-}
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        // Swap pivot to the correct position
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        return i;
+    }
+
     public static void main(String[] args) {
-       int arr[]={6,3,8,9,2,5};
-       QuickSort(arr, 0, arr.length-1); 
-       printArry(arr);
+        int arr[] = { 6, 3, 8, 9, 2, 5 };
+        QuickSort(arr, 0, arr.length - 1);
+        printArry(arr);
     }
 }
